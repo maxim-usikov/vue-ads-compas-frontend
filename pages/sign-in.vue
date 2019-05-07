4778be02ecb76b4db10d43443c29cf92875e8680
Maxim Usikov <mu.usikov@gmail.com>
2019-05-07 14:20:23 +0300

    feat(pages): add sign-in

---
+++ pages/sign-in.vue
@@ -0,0 +1,18 @@
+<template>
+  <VFlex xs12 sm8 md4>
+    <SignInCard />
+  </VFlex>
+</template>
+
+<script>
+import SignInCard from '@/components/Auth/SignInCard'
+
+export default {
+  middleware: 'guest',
+  layout: 'centered',
+
+  components: {
+    SignInCard
+  }
+}
+</script>
