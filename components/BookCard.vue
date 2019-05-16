434bed86feacd5d41ff1b7a13adcc5fbffd91e6f
Maxim Usikov <mu.usikov@gmail.com>
2019-05-16 12:18:39 +0300

    feat(components): add `BookCard`

---
+++ components/BookCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <VCard color="gray" hover>
+    <VLayout row>
+      <VFlex>
+        <VCardTitle primary-title>
+          <div>
+            <div class="headline">{{ title }}</div>
+          </div>
+        </VCardTitle>
+      </VFlex>
+    </VLayout>
+
+    <VDivider light />
+
+    <VCardActions class="pa-3">
+      <VChip v-for="author in authors" :key="author.id" class="mr-3">{{
+        author.name
+      }}</VChip>
+    </VCardActions>
+  </VCard>
+</template>
+
+<script>
+export default {
+  name: 'BookCard',
+
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    authors: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
