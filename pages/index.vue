9dd4ee6016e56d3f82d7312e377b47a5c39f1ed6
Maxim Usikov <mu.usikov@gmail.com>
2019-05-17 11:28:12 +0300

    feat(pages): use book author filter form
    and add show no books if it empty

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,36 @@
 <template>
   <section>
-    <BookCard
-      v-for="book in books.data"
-      :key="book.id"
-      :title="book.title"
-      :authors="book.authors"
-      class="mb-3"
-    />
-    <VPagination
-      :value="currentPage"
-      :length="totalPages"
-      :total-visible="7"
-      @input="handlePage"
-    />
+    <BookAuthorFilterForm class="mb-3" @filter="handleFilter" />
+
+    <template v-if="books.data.length > 0">
+      <BookCard
+        v-for="book in books.data"
+        :key="book.id"
+        :title="book.title"
+        :authors="book.authors"
+        class="mb-3"
+      />
+
+      <VPagination
+        :value="currentPage"
+        :length="totalPages"
+        :total-visible="7"
+        @input="handlePage"
+      />
+    </template>
+    <h1 v-else>No books</h1>
   </section>
 </template>
 
 <script>
+import BookAuthorFilterForm from '@/components/BookAuthorFilterForm'
 import BookCard from '@/components/BookCard'
 
 export default {
   layout: 'app',
 
   components: {
+    BookAuthorFilterForm,
     BookCard
   },
 
@@ -50,11 +58,15 @@ export default {
     return { books }
   },
 
-  watchQuery: ['page'],
+  watchQuery: ['page', 'title', 'authorName'],
 
   methods: {
     handlePage(page) {
-      this.$router.push({ query: { page } })
+      this.$router.push({ query: { ...this.$route.query, page } })
+    },
+
+    handleFilter({ title, authorName }) {
+      this.$router.push({ query: { title, authorName } })
     }
   }
 }
